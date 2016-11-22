d419155904733fa777391cdefc87469f09a8fd36
mkevinevans <mkevinevans@gmail.com>
2016-11-22 15:17:51 -0500

    Done.

---
+++ src/components/band_index_component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+ 
+const BandIndex = (props) => {
+  return(
+    <li>
+      {props.title}
+    </li>
+  )
+}
+ 
+export default BandIndex;
---
+++ src/components/band_input_component.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import BandIndex  from './band_index_component';
+ 
+export default class BandInput extends React.Component {
+	constructor(props){
+		super(props)
+		this.onSubmit = this.onSubmit.bind(this)
+	}
+
+	onSubmit(event){
+		event.preventDefault()
+		let action = {type: "ADD_BAND", payload: {title: event.target.children[1].value}}
+		this.props.store.dispatch(action)
+		}
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.onSubmit}>
+                    <label>Enter Band: </label>
+                    <input />
+                    <button type="submit">Submit</button>
+                </form>
+	            <div>
+				<h1>Bands</h1>
+					 <ul>
+	                {this.props.store.getState().map((band, i) => <BandIndex key={i} band={band} />)}
+	            	</ul>
+				</div>
+			</div>
+        )
+    }
+}
